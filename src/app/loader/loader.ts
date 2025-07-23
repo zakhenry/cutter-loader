947fc4b0eda344890ef308a35b63ae029da9f7bc
Zak Henry <zak.henry@gmail.com>
2025-07-24 11:43:29 +1200

    code cleanup

--- src/app/loader/loader.ts
+++ src/app/loader/loader.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-loader', imports: [], template: `
+    selector: 'app-loader',
+    imports: [],
+    template: `
         <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="-500 -500 1000 1000"
+                [attr.viewBox]="[-viewboxWidth/2, -viewboxWidth/2, viewboxWidth, viewboxWidth]"
                 transform="rotate(-90 0 0)"
         >
 
@@ -31,11 +33,11 @@ import {Component} from '@angular/core';
                     </g>
     
                     <mask id="cutter-mask">
-                        <rect x="-500" y="-500" width="1000" height="1000" fill="white"/>
+                        <rect [attr.x]="-viewboxWidth/2" [attr.y]="-viewboxWidth/2" [attr.width]="viewboxWidth" [attr.height]="viewboxWidth" fill="white"/>
                         <use href="#cutter-raw"/>
                     </mask>
     
-                    <filter id="stroke-sim" filterUnits="userSpaceOnUse" x="-500" y="-500" width="1000" height="1000">
+                    <filter id="stroke-sim" filterUnits="userSpaceOnUse" [attr.x]="-viewboxWidth/2" [attr.y]="-viewboxWidth/2" [attr.width]="viewboxWidth" [attr.height]="viewboxWidth">
                         <feMorphology in="SourceAlpha" operator="dilate" radius="4" result="dilated"/>
                         <feComposite in="dilated" in2="SourceAlpha" operator="xor" result="outline"/>
                         <feFlood flood-color="white" result="strokeFill"/>
@@ -69,37 +71,42 @@ import {Component} from '@angular/core';
                 </circle>
             </g>
         </svg>
-    `, styles: ``
+    `
 })
 export class Loader {
 
+    viewboxWidth = 1000
+
+    // inputs
     sides = 5
     duration = 5
     stepoverPercent = 30; // note this must be less than 50% due to how the masking currently works
-    cutterDiameter = 200;
+
+    cutterDiameter = this.viewboxWidth / 5;
+    targetDiameter = this.viewboxWidth / 2;
+
+    // calculated values
     stepover = this.cutterDiameter * (this.stepoverPercent / 100);
-
-    targetDiameter = 500;
     stockDiameter = this.targetDiameter + (2 * this.stepover) / Math.cos(Math.PI / this.sides);
+    animation = this.generateAnimationFrames(this.sides, this.targetDiameter, this.stockDiameter);
+    cutterPath = this.generateOffsetPolyLine(this.sides, this.targetDiameter/2, this.cutterDiameter/2);
 
-    generatePolyPoints(sides: number, radius: number) {
+    generatePolyPoints(sides: number, radius: number): number[][] {
         return Array.from({length: sides}).map((_, vert) => {
-            // points are intentionally duplicated as they will animate independently at the cut edge
             const theta = vert * (Math.PI * 2) / sides;
-            return [(Math.cos(theta) * radius).toFixed(2), (Math.sin(theta) * radius).toFixed(2),]
+            return [
+                (Math.cos(theta) * radius),
+                (Math.sin(theta) * radius),
+            ]
         })
     }
 
     generateOffsetPolyLine(sides: number, radius: number, offset: number): string {
 
-        const points = Array.from({ length: sides }, (_, i) => {
-            const theta = (2 * Math.PI * i) / sides;
-            return { x: Math.cos(theta), y: Math.sin(theta) };
-        });
+        const points = this.generatePolyPoints(sides, radius);
 
-        const vertices = points.map(p => ({ x: p.x * radius, y: p.y * radius }));
-        const normals = points.map((_, i) => {
-            const a = vertices[i];
+        const vertices = points.map(([x, y]) => ({ x, y }));
+        const normals = vertices.map((a, i) => {
             const b = vertices[(i + 1) % sides];
             const dx = b.x - a.x, dy = b.y - a.y;
             const len = Math.hypot(dx, dy);
@@ -116,11 +123,11 @@ export class Loader {
         });
 
         for (let i = 0; i < sides; i++) {
-            const v = vertices[i];
-            const n1 = normals[i];
+            const vertex = vertices[i];
+            const normal = normals[i];
 
-            const out = { x: v.x + n1.x * offset, y: v.y + n1.y * offset };
-            const inc = { x: vertices[(i + 1) % sides].x + n1.x * offset, y: vertices[(i + 1) % sides].y + n1.y * offset };
+            const out = { x: vertex.x + normal.x * offset, y: vertex.y + normal.y * offset };
+            const inc = { x: vertices[(i + 1) % sides].x + normal.x * offset, y: vertices[(i + 1) % sides].y + normal.y * offset };
 
             d += arcTo(out);
             d += lineTo(inc);
@@ -129,54 +136,55 @@ export class Loader {
         return d + 'Z';
     }
 
-
-    animation = this.generateAnimationFrames(this.sides, this.targetDiameter, this.stockDiameter);
-
-    cutterPath = this.generateOffsetPolyLine(this.sides, this.targetDiameter/2, this.cutterDiameter/2);
-
-    // polyPoints = this.generatePolyPoints(this.sides, this.maxDiameter/2)
-    // polyPoints2 = this.generatePolyPoints(this.sides, this.minDiameter/2)
-
     generateAnimationFrames(sides: number, minDiameter: number, maxDiameter: number) {
 
-        const allPoints = Array.from({length: sides}).map((_, index) => {
-            const diameter = minDiameter + (index % sides) * (maxDiameter - minDiameter) / (sides-1);
+        const allPoints = Array.from({ length: sides }).map((_, index) => {
+            const diameter = minDiameter + (index % sides) * (maxDiameter - minDiameter) / (sides - 1);
             return this.generatePolyPoints(sides, diameter / 2)
-        })
-
-        const rotatedPoints = Array.from({length: sides + 1}).flatMap((_, keyframeIndex) => {
-            const rotated = Array.from({length: sides}).map((_, sideIndex) => {
-                const rotatedOffset = (sides + keyframeIndex - sideIndex - 1) % sides;
-
-                const targetPoint = rotatedOffset === 0 ? allPoints.at(-1)![sideIndex]: allPoints[rotatedOffset][sideIndex];
-
-                return [
-                    allPoints[rotatedOffset][sideIndex],
-                    targetPoint,
-                ];
-            });
-
-            const rotatedOffset = Array.from({length: sides}).map((_, sideIndex) => {
-                const rotatedOffset = (sides + keyframeIndex - sideIndex - 1) % sides;
-
-                const targetPoint = rotatedOffset === sides - 1 ? allPoints.at(0)![sideIndex]: allPoints[rotatedOffset][sideIndex];
-
-                return [
-                    targetPoint,
-                    allPoints[rotatedOffset][sideIndex],
-                ];
-            });
-
-            return [rotated, rotatedOffset]
+                .map(p => p.map(p => p.toFixed(0)));
         });
 
+        // Helper function to get modified vertex (if applicable, returns null if no modification required)
+        function getModifiedVertex(rotatedOffset: number, sideIndex: number, trailingCoordinate: boolean) {
+            if (trailingCoordinate && rotatedOffset === sides - 1) {
+                return allPoints[0][sideIndex];
+            }
 
-        const keyTimes = Array.from({length: sides }).flatMap((_, index) => [index / sides, index / sides]).join("; ") + '; 1; 1';
+            if (!trailingCoordinate && rotatedOffset === 0) {
+                return allPoints[allPoints.length - 1][sideIndex];
+            }
+
+            return null;
+        }
+
+        function computeConnectionPairs(keyframeIndex: number, trailingCoordinate: boolean) {
+            return Array.from({ length: sides }).map((_, sideIndex) => {
+                const rotatedOffset = (sides + keyframeIndex - sideIndex - 1) % sides;
+                const actualVertex = allPoints[rotatedOffset][sideIndex];
+                const modifiedVertex = getModifiedVertex(rotatedOffset, sideIndex, trailingCoordinate) ?? actualVertex;
+
+                return trailingCoordinate
+                    ? [modifiedVertex, actualVertex]
+                    : [actualVertex, modifiedVertex];
+            });
+        }
+
+        const rotatedPoints = Array.from({ length: sides + 1 })
+            .flatMap((_, keyframeIndex) => {
+                return [
+                    computeConnectionPairs(keyframeIndex, false),
+                    computeConnectionPairs(keyframeIndex, true)
+                ];
+            });
+
+        const keyTimes = Array.from({ length: sides })
+            .flatMap((_, index) => [index / sides, index / sides])
+            .join("; ") + '; 1; 1';
 
         return {
             keyTimes,
             values: rotatedPoints.join(';\n')
-        }
+        };
 
     }
 
